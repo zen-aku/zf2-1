60b5bc783952ba9cba5231c7125639be02766584
algerd <algerd>
2014-10-21 13:50:43 +0300

    db

---
+++ Learning_Modules/ZendDb/autoload_classmap.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    // 'полное имя класса' => 'абсолютный путь к файлу с классом'
+    'ZendDb\Module' => __DIR__.'/Module.php',
+	
+	// controllers
+    'ZendDb\Controller\IndexController' => __DIR__.'/src/ZendDb/Controller/IndexController.php',
+	'ZendDb\Controller\CreateDbController' => __DIR__.'/src/ZendDb/Controller/CreateDbController.php',
+    'ZendDb\Controller\AdapterController' => __DIR__.'/src/ZendDb/Controller/AdapterController.php',
+	'ZendDb\Controller\SqlController' => __DIR__.'/src/ZendDb/Controller/SqlController.php',
+    'ZendDb\Controller\DdlController' => __DIR__.'/src/ZendDb/Controller/DdlController.php',
+    
+	// services
+	
+);
