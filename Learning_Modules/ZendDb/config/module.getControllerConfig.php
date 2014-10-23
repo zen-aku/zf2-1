aeed64d9bb8a426f700742c99033632318bdf7ee
algerd <algerd>
2014-10-23 14:35:04 +0300

    db

---
+++ Learning_Modules/ZendDb/config/module.getControllerConfig.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Controllers
+ */
+return array(
+
+	'invokables' => array(
+		'ZendDb\Controller\Index' => 'ZendDb\Controller\IndexController',
+		'ZendDb\Controller\CreateDb' => 'ZendDb\Controller\CreateDbController',
+        'ZendDb\Controller\Adapter' => 'ZendDb\Controller\AdapterController',
+        'ZendDb\Controller\Sql' => 'ZendDb\Controller\SqlController',
+        'ZendDb\Controller\Ddl' => 'ZendDb\Controller\DdlController',
+	),
+
+	'factories' => array(
+
+	),
+
+);
