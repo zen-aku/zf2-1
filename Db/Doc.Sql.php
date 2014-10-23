9ffac8f53b79d52ad395307601d1d5afc55caaec
algerd <algerd>
2014-10-23 11:16:39 +0300

    в

--- Db/Doc.Sql.php
+++ Db/Doc.Sql.php
@@ -8,9 +8,9 @@ Zend\Db\Sql\Sql формирует строку Sql-запроса в соотв
 существенно утяжеляется, теряет читабельность, усложняется как внаписании так и в понимании.
 В таких ситуациях целесообразно ипользовать подготовленные запросы с прямым Sql-кодом 
 		или рассмотреть использование запросов c помощью Doctrine2
-				
-API Sql-запросов:
-
+-------------------				
+    API Sql:
+-------------------
 class Sql {
 	__construct(AdapterInterface $adapter, $table = null, Platform\AbstractPlatform $sqlPlatform = null)
 	AdapterInterface getAdapter()
@@ -52,6 +52,10 @@ class Select extends AbstractSql implements SqlInterface, PreparableSqlInterface
 }
 
 class Insert implements SqlInterface, PreparableSqlInterface {
+    protected $specifications = array(
+        self::SPECIFICATION_INSERT => 'INSERT INTO %1$s (%2$s) VALUES (%3$s)',
+        self::SPECIFICATION_SELECT => 'INSERT INTO %1$s %2$s %3$s',
+    );
     const VALUES_MERGE = 'merge';
     const VALUES_SET   = 'set';
 
@@ -64,6 +68,10 @@ class Insert implements SqlInterface, PreparableSqlInterface {
 }
 
 class Update extends AbstractSql implements SqlInterface, PreparableSqlInterface {
+    protected $specifications = array(
+        self::SPECIFICATION_UPDATE => 'UPDATE %1$s SET %2$s',
+        self::SPECIFICATION_WHERE => 'WHERE %1$s'
+    );
     const VALUES_MERGE = 'merge';
     const VALUES_SET   = 'set';
 
@@ -77,6 +85,10 @@ class Update extends AbstractSql implements SqlInterface, PreparableSqlInterface
 }
 
 class Delete {
+    protected $specifications = array(
+        self::SPECIFICATION_DELETE => 'DELETE FROM %1$s',
+        self::SPECIFICATION_WHERE => 'WHERE %1$s'
+    );
     public $where; // @param Where $where
     __construct($table = null);
     from($table);
@@ -85,10 +97,43 @@ class Delete {
     getSqlString(PlatformInterface $adapterPlatform = null) 
 }
 
+class Expression implements ExpressionInterface {
+    const PLACEHOLDER = '?';
+    __construct($expression = '', $parameters = null, array $types = array())
+    setExpression($expression)
+    getExpression()
+    setParameters($parameters)    
+    getParameters()
+    setTypes(array $types)    
+    getTypes()
+    array getExpressionData()          
+}
 
-/*
- *  Для детального WHERE надо использовать класс Predicate
- */
+class Literal implements ExpressionInterface {
+    __construct($literal = '')
+    setLiteral($literal)     
+    getLiteral()
+    array getExpressionData()           
+}
+
+--------------------------
+    API Predicate 
+--------------------------
+class PredicateSet implements PredicateInterface, Countable {
+    const COMBINED_BY_AND = 'AND';
+    const OP_AND          = 'AND';
+    const COMBINED_BY_OR  = 'OR';
+    const OP_OR           = 'OR';
+    __construct(array $predicates = null, $defaultCombination = self::COMBINED_BY_AND)
+    addPredicate(PredicateInterface $predicate, $combination = null)
+    addPredicates($predicates, $combination = self::OP_AND)    
+    getPredicates() 
+    orPredicate(PredicateInterface $predicate)   
+    andPredicate(PredicateInterface $predicate)
+    array getExpressionData()
+    count()    
+}
+
 class Predicate extends PredicateSet {
     public $and;
     public $or;
@@ -117,10 +162,112 @@ class Predicate extends PredicateSet {
     getPredicates();
     orPredicate(PredicateInterface $predicate);
     andPredicate(PredicateInterface $predicate);
-    getExpressionData();
+    array getExpressionData();
     count();
 }
 
+class Where extends Predicate\Predicate { 
+}
+
+class Having extends Predicate\Predicate {
+}
+
+class Expression extends BaseExpression implements PredicateInterface{
+    __construct($expression = null, $valueParameter = null /*[, $valueParameter, ... ]*/)
+}
+--------------------------
+    API Predicate-Command 
+--------------------------       
+class Operator implements PredicateInterface {
+    const OPERATOR_EQUAL_TO                  = '=';
+    const OP_EQ                              = '=';
+    const OPERATOR_NOT_EQUAL_TO              = '!=';
+    const OP_NE                              = '!=';
+    const OPERATOR_LESS_THAN                 = '<';
+    const OP_LT                              = '<';
+    const OPERATOR_LESS_THAN_OR_EQUAL_TO     = '<=';
+    const OP_LTE                             = '<=';
+    const OPERATOR_GREATER_THAN              = '>';
+    const OP_GT                              = '>';
+    const OPERATOR_GREATER_THAN_OR_EQUAL_TO  = '>=';
+    const OP_GTE                             = '>=';
+    __construct($left = null, $operator = self::OPERATOR_EQUAL_TO, $right = null, $leftType = self::TYPE_IDENTIFIER, $rightType = self::TYPE_VALUE)
+    setLeft($left)    
+    getLeft()    
+    setLeftType($type)
+    getLeftType()    
+    setOperator($operator)
+    getOperator()    
+    setRight($value)
+    getRight()    
+    setRightType($type)
+    getRightType()    
+    array getExpressionData()    
+} 
+
+class Between implements PredicateInterface {
+    protected $specification = '%1$s BETWEEN %2$s AND %3$s';
+    __construct($identifier = null, $minValue = null, $maxValue = null)
+    setIdentifier($identifier)
+    getIdentifier()    
+    setMinValue($minValue)
+    getMinValue()    
+    setMaxValue($maxValue)
+    getMaxValue()    
+    setSpecification($specification)
+    getSpecification()    
+    array getExpressionData()    
+}
+
+class In implements PredicateInterface {
+    protected $specification = '%s IN %s';
+    __construct($identifier = null, $valueSet = null)
+    setIdentifier($identifier)    
+    getIdentifier()
+    setValueSet($valueSet)    
+    getValueSet()
+    array getExpressionData()           
+}
+
+class IsNull implements PredicateInterface {
+    protected $specification = '%1$s IS NULL';
+    __construct($identifier = null)
+    setIdentifier($identifier)    
+    getIdentifier()
+    setSpecification($specification)    
+    getSpecification()
+    array getExpressionData()    
+}
+
+class IsNotNull extends IsNull {
+    protected $specification = '%1$s IS NOT NULL';
+}
+
+class Like implements PredicateInterface {
+    protected $specification = '%1$s LIKE %2$s';
+    __construct($identifier = null, $like = null)
+    setIdentifier($identifier)
+    getIdentifier()
+    setLike($like)    
+    getLike()    
+    setSpecification($specification)
+    getSpecification()  
+    array getExpressionData()           
+}
+
+class NotIn extends In {
+    protected $specification = '%s NOT IN %s';
+}
+
+class NotLike extends Like {
+    protected $specification = '%1$s NOT LIKE %2$s';
+}
+
+class Literal extends BaseLiteral implements PredicateInterface { }
+
+
 Некоторые методы класса Predicate можно заменить классами-командами из директории Zend\Db\Sql\Predicate:
 Between, Expression, In, IsNotNull, isNull, Like, Literal, NotIn, NotKike, Operator (=,>,< и т.д.), но это утяжелит код.
-Рекомендуется для сложных запросов Where применять класс Predicate и при необходимости дополнять его классами-командами из директории Zend\Db\Sql\Predicate		
+Рекомендуется для сложных запросов Where применять класс Predicate и при необходимости дополнять его классами-командами из директории Zend\Db\Sql\Predicate	
+    
+    
