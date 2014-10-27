d7032cbb6ac1a1812b57f73ced54d19545bece93
Alexander <algerd75@mail.ru>
2014-10-27 21:57:26 +0200

    d

---
+++ DbGenerator/Db/Adapter/Platform/Mysql.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Zend\Db\Adapter\Platform;
+
+use Zend\Db\Adapter\Driver\DriverInterface;
+use Zend\Db\Adapter\Driver\Pdo;
+use Zend\Db\Adapter\Exception;
+
+class Mysql implements PlatformInterface
+{
+    /**
+	 *  @var \PDO 
+	 */
+    protected $resource = null;
+
+    public function __construct($driver = null) {
+        if ($driver) {
+            $this->setDriver($driver);
+        }
+    }
+
+    /**
+     * @param \Zend\Db\Adapter\Driver\Mysqli\Mysqli|\Zend\Db\Adapter\Driver\Pdo\Pdo||\mysqli|\PDO $driver
+     * @throws \Zend\Db\Adapter\Exception\InvalidArgumentException
+     * @return $this
+     */
+    public function setDriver($driver) {
+        // handle Zend\Db drivers
+//!!!	// нужна проверка на принадлежность драйвера к платформе mysql
+        if (($driver instanceof Pdo\Pdo) ) {
+            $this->resource = $driver;
+            return $this;
+        }
+        throw new Exception\InvalidArgumentException('$driver must be MySQL PDO instance');
+    }
+
+    /**
+     * Get name
+     * @return string
+     */
+    public function getName() {
+        return 'MySQL';
+    }
+
+    /**
+     * Get quote identifier symbol
+     * @return string
+     */
+    public function getQuoteIdentifierSymbol() {
+        return '`';
+    }
+
+    /**
+     * Quote identifier
+     *
+     * @param  string $identifier
+     * @return string
+     */
+    public function quoteIdentifier($identifier)
+    {
+        return '`' . str_replace('`', '``', $identifier) . '`';
+    }
+
+    /**
+     * Quote identifier chain
+     *
+     * @param string|string[] $identifierChain
+     * @return string
+     */
+    public function quoteIdentifierChain($identifierChain)
+    {
+        $identifierChain = str_replace('`', '``', $identifierChain);
+        if (is_array($identifierChain)) {
+            $identifierChain = implode('`.`', $identifierChain);
+        }
+        return '`' . $identifierChain . '`';
+    }
+
+    /**
+     * Get quote value symbol
+     *
+     * @return string
+     */
+    public function getQuoteValueSymbol()
+    {
+        return '\'';
+    }
+
+    /**
+     * Quote value
+     *
+     * @param  string $value
+     * @return string
+     */
+    public function quoteValue($value)
+    {
+        if ($this->resource instanceof DriverInterface) {
+            $this->resource = $this->resource->getConnection()->getResource();
+        }
+        if ($this->resource instanceof \mysqli) {
+            return '\'' . $this->resource->real_escape_string($value) . '\'';
+        }
+        if ($this->resource instanceof \PDO) {
+            return $this->resource->quote($value);
+        }
+        trigger_error(
+            'Attempting to quote a value in ' . __CLASS__ . ' without extension/driver support '
+                . 'can introduce security vulnerabilities in a production environment.'
+        );
+        return '\'' . addcslashes($value, "\x00\n\r\\'\"\x1a") . '\'';
+    }
+
+    /**
+     * Quote Trusted Value
+     *
+     * The ability to quote values without notices
+     *
+     * @param $value
+     * @return mixed
+     */
+    public function quoteTrustedValue($value)
+    {
+        if ($this->resource instanceof DriverInterface) {
+            $this->resource = $this->resource->getConnection()->getResource();
+        }
+        if ($this->resource instanceof \mysqli) {
+            return '\'' . $this->resource->real_escape_string($value) . '\'';
+        }
+        if ($this->resource instanceof \PDO) {
+            return $this->resource->quote($value);
+        }
+        return '\'' . addcslashes($value, "\x00\n\r\\'\"\x1a") . '\'';
+    }
+
+    /**
+     * Quote value list
+     *
+     * @param string|string[] $valueList
+     * @return string
+     */
+    public function quoteValueList($valueList)
+    {
+        if (!is_array($valueList)) {
+            return $this->quoteValue($valueList);
+        }
+
+        $value = reset($valueList);
+        do {
+            $valueList[key($valueList)] = $this->quoteValue($value);
+        } while ($value = next($valueList));
+        return implode(', ', $valueList);
+    }
+
+    /**
+     * Get identifier separator
+     *
+     * @return string
+     */
+    public function getIdentifierSeparator()
+    {
+        return '.';
+    }
+
+    /**
+     * Quote identifier in fragment
+     *
+     * @param  string $identifier
+     * @param  array $safeWords
+     * @return string
+     */
+    public function quoteIdentifierInFragment($identifier, array $safeWords = array())
+    {
+        // regex taken from @link http://dev.mysql.com/doc/refman/5.0/en/identifiers.html
+        $parts = preg_split('#([^0-9,a-z,A-Z$_])#', $identifier, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+        if ($safeWords) {
+            $safeWords = array_flip($safeWords);
+            $safeWords = array_change_key_case($safeWords, CASE_LOWER);
+        }
+        foreach ($parts as $i => $part) {
+            if ($safeWords && isset($safeWords[strtolower($part)])) {
+                continue;
+            }
+            switch ($part) {
+                case ' ':
+                case '.':
+                case '*':
+                case 'AS':
+                case 'As':
+                case 'aS':
+                case 'as':
+                    break;
+                default:
+                    $parts[$i] = '`' . str_replace('`', '``', $part) . '`';
+            }
+        }
+        return implode('', $parts);
+    }
+}
--- DbGenerator/DbGenerator/autoload_classmap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-
-    'DbGenerator\Module' => __DIR__.'/Module.php',
-	
-    //'DbGenerator\Mysql\Test' => __DIR__.'/Mysql/Test.php',
-		
-);
