bb29b4aa648b3f24bc39276d5d2201ba7831cabc
algerd <algerd>
2014-10-29 14:25:06 +0300

    w

---
+++ Learning_Modules/ZendDb/autoload_classmap.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+    // 'полное имя класса' => 'абсолютный путь к файлу с классом'
+    'ZendDb\Module' => __DIR__.'/Module.php',
+	
+	// controllers
+    'ZendDb\Controller\IndexController' => __DIR__.'/src/ZendDb/Controller/IndexController.php',
+	'ZendDb\Controller\CreateDbController' => __DIR__.'/src/ZendDb/Controller/CreateDbController.php',
+    'ZendDb\Controller\AdapterController' => __DIR__.'/src/ZendDb/Controller/AdapterController.php',
+	'ZendDb\Controller\SqlController' => __DIR__.'/src/ZendDb/Controller/SqlController.php',
+    'ZendDb\Controller\DdlController' => __DIR__.'/src/ZendDb/Controller/DdlController.php',
+    'ZendDb\Controller\MydbController' => __DIR__.'/src/ZendDb/Controller/MydbController.php',
+    'ZendDb\Controller\DbLightController' => __DIR__.'/src/ZendDb/Controller/DbLightController.php',
+    
+	// services
+    'ZendDb\Service\SqlFactory' => __DIR__.'/src/ZendDb/Service/SqlFactory.php',
+    'ZendDb\Service\SqlFactoryAdapterAware' => __DIR__.'/src/ZendDb/Service/SqlFactoryAdapterAware.php',
+	
+    
+    
+);
---
+++ Learning_Modules/ZendDb/config/module.getControllerConfig.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Controllers
+ */
+return array(
+
+	'invokables' => array(
+		'ZendDb\Controller\Index' => 'ZendDb\Controller\IndexController',
+		'ZendDb\Controller\CreateDb' => 'ZendDb\Controller\CreateDbController',
+        'ZendDb\Controller\Adapter' => 'ZendDb\Controller\AdapterController',
+        'ZendDb\Controller\Sql' => 'ZendDb\Controller\SqlController',
+        'ZendDb\Controller\Ddl' => 'ZendDb\Controller\DdlController',
+        'ZendDb\Controller\Mydb' => 'ZendDb\Controller\MydbController',
+        'ZendDb\Controller\DbLight' => 'ZendDb\Controller\DbLightController',
+	),
+
+	'factories' => array(
+
+	),
+
+);
